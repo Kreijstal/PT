2730cb12ef4854ecb8032ae24f34bab985c8eca6
Rainb <elektrischrainbow@gmail.com>
2016-09-16 14:02:08 +0000

    is buffer went missing

--- libraries.js
+++ libraries.js
@@ -65145,6 +65145,28 @@ function iota(n) {
 }
 
 module.exports = iota},
+"is-buffer/index.js": function(require,exports,module){/*!
+ * Determine if an object is a Buffer
+ *
+ * @author   Feross Aboukhadijeh <feross@feross.org> <http://feross.org>
+ * @license  MIT
+ */
+
+// The _isBuffer check is for Safari 5-7 support, because it's missing
+// Object.prototype.constructor. Remove this eventually
+module.exports = function (obj) {
+  return obj != null && (isBuffer(obj) || isSlowBuffer(obj) || !!obj._isBuffer)
+}
+
+function isBuffer (obj) {
+  return !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+}
+
+// For Node v0.10 support. Remove this eventually.
+function isSlowBuffer (obj) {
+  return typeof obj.readFloatLE === 'function' && typeof obj.slice === 'function' && isBuffer(obj.slice(0, 0))
+}
+},
         "cwise-compiler/compiler.js":function(require,exports,module){"use strict"
 
 var createThunk = require(getCodeName("cwise-compiler/lib/thunk.js"))
